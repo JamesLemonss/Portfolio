ad777582cb11190369d1530f08d98b3379cfc662
James Latanna <jamesgetgit@gmail.com>
2025-06-03 23:03:41 +0900

    update

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NavBar from './sections/navbar';
+
+const App = () => {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      <NavBar/>
+      {/* Hero */}
+      {/* About */}
+      {/* Projects */}
+      {/* Experience */}
+      {/* Testimonial */}
+      {/* Contact */}
+      {/* Footer */}
+      </div>
+  );
+};
+
+export default App;
