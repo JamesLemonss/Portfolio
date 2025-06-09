18effdbb51a3bb6f3cadd32f245a73a32b54b8d5
James Latanna <jamesgetgit@gmail.com>
2025-06-10 01:44:21 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,32 @@ import Hero from './sections/Hero';
 import About from './sections/About';
 import Projects from './sections/Projects';
 import Experiences from './sections/Experiences';
+import Testimonial from './sections/Testimonial';
+import Contact from './sections/Contact';
 const App = () => {
   return (
     <div className='container mx-auto max-w-7xl'>
       <NavBar/>
-      <Hero />
-      <About />
-      <Projects />
-      <Experiences />
-      <section className='min-h-screen' />
-      <section className='min-h-screen' />
-      {/* Experience */}
-      {/* Testimonial */}
-      {/* Contact */}
-      {/* Footer */}
+      <div id="home">
+        <Hero />
       </div>
+      <div id="about">
+        <About />
+      </div>
+      <div id="work">
+        <Projects />
+      </div>
+      <div id="experience">
+        <Experiences />
+      </div>
+      <div id="testimonial">
+        <Testimonial />
+      </div>
+      <div id='contact'>
+        <Contact />
+      </div>
+      {/* Footer */}
+    </div>
   );
 };
 
